169a699204d2ba9fe55e956562ed453dda973b8d
atangster <ashley.t1248@gmail.com>
2025-08-21 23:32:57 +1000

    run dart format

--- lib/services/content_search_service.dart
+++ lib/services/content_search_service.dart
@@ -233,7 +233,8 @@ class ContentSearchService {
         );
 
         allContent.addAll(
-            movieResults.map((movie) => ContentItem.fromMovieJson(movie)),);
+          movieResults.map((movie) => ContentItem.fromMovieJson(movie)),
+        );
       }
     } catch (e) {
       // Continue even if movie genre search fails.
--- lib/services/content_service.dart
+++ lib/services/content_service.dart
@@ -231,7 +231,9 @@ class ContentService {
   // Gets detailed information about content (auto-detects type).
 
   Future<ContentItem> getContentDetails(
-      int contentId, ContentType contentType,) async {
+    int contentId,
+    ContentType contentType,
+  ) async {
     if (contentType == ContentType.movie) {
       return getMovieDetails(contentId);
     } else {

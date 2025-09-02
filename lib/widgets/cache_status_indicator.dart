abb1151723cca8767235c1ebbbaf6fa95532f5e8
atangster <ashley.t1248@gmail.com>
2025-09-02 12:16:02 +1000

    fix import order

--- lib/widgets/cache_status_indicator.dart
+++ lib/widgets/cache_status_indicator.dart
@@ -30,8 +30,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 
-import 'package:moviestar/providers/cached_movie_service_provider.dart';
 import 'package:moviestar/my_home_page.dart';
+import 'package:moviestar/providers/cached_movie_service_provider.dart';
 import 'package:moviestar/services/favorites_service.dart';
 import 'package:moviestar/services/hive_movie_cache_service.dart';
 import 'package:moviestar/widgets/cache_feedback_widget.dart';
